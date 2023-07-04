bce46cf468fb066915aa42c1dfaa30b25a9ae11c
NBK-01 <nayefkanana03@gmail.com>
2023-07-04 21:07:48 +0300

    test

--- app/gallery/layout.tsx
+++ app/gallery/layout.tsx
@@ -2,14 +2,17 @@ import React from "react";
 import "@/styles/globals.css"
 
 export default function GalleryLayout({
-    children, 
+    children,
+    modal, 
   }: {
     children: React.ReactNode;
+    modal: React.ReactNode;
   }) {
     return (
       
      <html>
        <body>
+       {modal}
           {children}
       </body>
      </html>
