be97df45f3124f3a2a3c3e51990e7642af94c614
NBK-01 <nayefkanana03@gmail.com>
2023-07-10 17:50:11 +0300

    test

--- lib/getPosts.ts
+++ lib/getPosts.ts
@@ -2,7 +2,11 @@ import prisma from "./prisma";
 
 export async function getPosts () {
     try {
-        const posts = await prisma.post.findMany()
+        const posts = await prisma.post.findMany({
+            orderBy : [
+                {id: "desc"}
+            ]
+        })
         return { posts }
     } catch(error) {
         return {error}
